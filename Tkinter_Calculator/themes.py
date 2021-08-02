3db9b37ed51049fc8afa87d3981b04632e2ddffa
Srijon <mandal.srijon@gmail.com>
2021-08-02 10:34:32 -0700

    Add files via upload

---
+++ Tkinter_Calculator/themes.py
@@ -0,0 +1,24 @@
+"""
+themes.py
+create hues for your calculator
+hexadecimal colors:  #HEXCODE
+base colors:         COLOR NAME
+grays:               grayNUM
+format:
+[background, foreground]
+submit themes:
+Email                    GitHub
+mandal.srijon@gmail.com     SrijonMaster1
+"""
+
+default = [                 # System
+    ['white', 'black'],     # Regular
+    ['#efad04', 'white'],   # Control
+    ['gray25', 'black']     # Operation
+]
+
+ocean = [                   # System
+    ['white', 'black'],     # Regular
+    ['#04c0ef', 'white'],   # Control
+    ['#f9df86', 'black']    # Operation
+]
